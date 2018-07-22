70327d0cfe85feb1c92a06d70c6617375904649e
sahluwalia17 <sahilahluwalia3@gmail.com>
2018-07-22 13:24:16 -0700

    updated URI algorithim

--- app/src/main/java/com/example/sahilahluwalia/spotconv/song.java
+++ app/src/main/java/com/example/sahilahluwalia/spotconv/song.java
@@ -6,8 +6,19 @@ public class song {
     String name;
 
     public String getURI(String link) {
-        int lastBackslash = link.lastIndexOf('/');
-        return link.substring(lastBackslash);
+        int lastBackslash = link.lastIndexOf('/') + 1;
+        int questionMark = link.indexOf('?');
+
+        String URI;
+        if (questionMark != -1)
+        {
+            URI = link.substring(lastBackslash,questionMark);
+        }
+        else
+        {
+            URI = link.substring(lastBackslash);
+        }
+        return URI;
     }
 
     public void setURI(String URI) {
