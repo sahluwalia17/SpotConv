bcb54acfb13b47f12dff6cf14eb3f0e9cff1606b
sahluwalia17 <sahilahluwalia3@gmail.com>
2018-08-11 21:48:09 -0700

    receiving json object

--- app/src/main/java/com/example/sahilahluwalia/spotconv/MainActivity.java
+++ app/src/main/java/com/example/sahilahluwalia/spotconv/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.sahilahluwalia.spotconv;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +10,18 @@ import android.widget.EditText;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity
 {
     private static final String CLIENT_ID = "82921fab329a4f7e96a9f4abb393e53a";
@@ -22,6 +31,8 @@ public class MainActivity extends AppCompatActivity
     EditText linkText;
     Button checkBtn;
     String authToken;
+    private final OkHttpClient mOkHttpClient = new OkHttpClient();
+    private Call mCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +53,29 @@ public class MainActivity extends AppCompatActivity
                 current.setURI(URI);
                 linkText.setText(URI);
 
+                final Request request = new Request.Builder()
+                        .url("https://api.spotify.com/v1/tracks/"+URI)
+                        .addHeader("Authorization","Bearer " + authToken)
+                        .build();
+
+                mCall = mOkHttpClient.newCall(request);
+
+                mCall.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        System.out.println("Failed to fetch data!");
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        try {
+                            final JSONObject jsonObject = new JSONObject(response.body().string());
+                            System.out.println(jsonObject.toString(3));
+                        } catch (JSONException e) {
+                            System.out.println("Failed to parse data: " + e);
+                        }
+                    }
+                });
             }
         });
     }
@@ -51,7 +85,5 @@ public class MainActivity extends AppCompatActivity
         super.onActivityResult(requestCode, resultCode, intent);
         AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
         authToken = response.getAccessToken();
-        System.out.println("&&&&&&&&&&&&" + authToken);
     }
-
 }
